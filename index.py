68b4197c77bae6988ecfe51d259f049e1e8bd65a
A. Foreman <handsofhoney@gmail.com>
2020-10-08 09:32:09 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# a function to calculate the square footage of a house
+# Reminder of Formula: Length X Width == Area
+
+def area(length, height):
+    sqft_house = (length(' ') + height(' '))
+    return(f"Yo! This is the {sqft_house} of my home")
+
+print(sqft_house)
+
+
+
+
+
+
